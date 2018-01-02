fdb003d94b84ceeea6e1cab1d000a3cdecd6e183
oikwsat <oikwsat@gmail.com>
2018-01-03 00:05:39 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const googlehome = require ('google-home-notifier');
+const cronJob    = require('cron').CronJob;
+const moment     = require("moment");
+
+// Setup Google Home
+googlehome.device('Google Home', 'ja');
+
+// Setup time signal
+const cronTime = '0 * 0,7-23 * * *';
+
+let job = new cronJob({
+  cronTime: cronTime,
+  start:    false,
+  timeZone: 'Asia/Tokyo',
+
+  onTick: function() {
+    let text = moment().hours() + '時です。';
+    googlehome.notify(text, function(res) {
+      console.log(res);
+    });
+  },
+
+  onComplete: function() {
+    console.log('onComplete');
+  }
+})
+
+job.start();
+//job.stop();
